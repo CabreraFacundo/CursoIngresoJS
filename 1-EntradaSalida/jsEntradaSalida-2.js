8d723b935374f07060f878588eb5c2561b11a66b
CabreraFacundo <40808725+CabreraFacundo@users.noreply.github.com>
2018-07-03 21:50:39 -0300

    Primer Guardado
    Son los ejercicios del 1 al 4.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,21 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//Comentario de una linea
+	/*
+	esto es un comentario en bloque,
+	sirve para poner apuntes
+	*/
+
+
+	var nombre ;
+
+	nombre = prompt("Ingrese nombre: ", "natalia natalia");
+	alert ("Su nombre es: " + nombre);
+
+	//alert(nombre);
+	//nombre= "Facundo";
+	//esto lo hicimos para probar el error "undefined".
 }
 
