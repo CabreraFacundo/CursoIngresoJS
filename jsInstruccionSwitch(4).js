9c7a05fecfe51f9ca09852042d91cdb3affb569d
CabreraFacundo <40808725+CabreraFacundo@users.noreply.github.com>
2018-07-13 22:06:57 -0300

    Ejercicios de SWITCH

---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,27 @@
+function mostrar()
+{
+  
+	var mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño){
+		case "Febrero":
+			alert("Tiene 28 dias.");
+			break;
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert("Tiene 31 dias.");
+			break;
+		default:
+			alert("Tiene 30 dias.");
+			break;
+	}
+	
+
+
+
+}//FIN DE LA FUNCIÓN
